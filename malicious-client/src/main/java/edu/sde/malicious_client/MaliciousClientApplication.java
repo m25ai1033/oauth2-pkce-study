bd448f29163c6ff285475c892f84fe0a3430bfe0
Deep Raj Singh <deep.singh@sunking.com>
2025-11-14 13:35:32 +0530

    Adding Spring structure for projects

---
+++ malicious-client/src/main/java/edu/sde/malicious_client/MaliciousClientApplication.java
@@ -0,0 +1,13 @@
+package edu.sde.malicious_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaliciousClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaliciousClientApplication.class, args);
+	}
+
+}
